72c9d626652410c322e3645e5b705ae235f19fa4
PixelRifts <njk777400@gmail.com>
2021-10-15 20:56:31 +0530

    More GCC fixes

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@ REM build script for engine
 @ECHO off
 SetLocal EnableDelayedExpansion
 
+SET cc=clang
+
 del /f generated.c
 
 REM Get's list of all C files
@@ -10,11 +12,12 @@ FOR /R %%f in (*.c) do (
 	SET c_filenames=!c_filenames! %%f
 )
 
-SET compiler_flags=-Wall -Wvarargs -Werror -Wno-unused-function -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers
+if %cc%==clang SET compiler_flags=-Wall -Wvarargs -Werror -Wno-unused-function -Wno-format-security -Wno-incompatible-pointer-types-discards-qualifiers
+if %cc%==gcc SET compiler_flags=-Wall -Wvarargs -Werror -Wno-unused-function -Wno-format-security -Wno-discarded-qualifiers
 SET include_flags=-Isource -Ilib/include
 SET linker_flags=-g -lshell32
 SET defines=-D_DEBUG -DCPLATEST -D_CRT_SECURE_NO_WARNINGS
 SET output=-o bin/cpcom.exe
 
 ECHO "Building cpcom.exe..."
-clang %c_filenames% %compiler_flags% %defines% %include_flags% %linker_flags% %output%
+%cc% %c_filenames% %compiler_flags% %defines% %include_flags% %linker_flags% %output%
