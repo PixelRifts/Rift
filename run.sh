f6e9af4c34501ab242f11084a19707bb76756dba
HilbertCurve <80780490+HilbertCurve@users.noreply.github.com>
2022-01-15 21:16:16 -0500

    added a few matrix operations

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 rm ./bin/generated
 
-gcc -o bin/generated generated.c || exit
+gcc -o bin/generated -DGL_GLEXT_PROTOTYPES generated.c -g -lm -lglfw3 -lglad -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl || exit
 ./bin/generated
